4dcc58faec2b55676c87f7209853479a3a68d7ab
Jeffry <jeffry1408.job@gmail.com>
2025-06-19 07:12:10 +0700

    edit link

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ const App = () => {
   return (
     <main role="application">
       <Calculator />
-      <a href="https://github.com/jeffamazed" target="_blank">
+      <a
+        href="https://github.com/jeffamazed"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="My logo and github"
+      >
         <img src={myLogo} alt="jeffamazed logo" className="logo" />
       </a>
     </main>
